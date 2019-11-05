b91ec6e87964f8a1c974778f03b8830094935e01
fasnimo <fasnimo@gmail.com>
2019-11-04 22:00:21 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230232412) do
+ActiveRecord::Schema.define(version: 20191102171535) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
